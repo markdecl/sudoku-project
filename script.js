e209fbfafcd2d7e843eb1e654620fb3f6eaabe3f
Mark de Courcy Ling <54800588+markdecl@users.noreply.github.com>
2021-12-14 20:18:23 +0000

    Add gameplay gif

--- script.js
+++ script.js
@@ -3,25 +3,6 @@ const newGameBtn = document.querySelector('#new-game');
 const restartBtn = document.querySelector('#restart');
 const hintBtn = document.querySelector('#hint');
 const autoSolveBtn = document.querySelector('#auto-solve');
-// const selectedCell = undefined
-
-// const boardState = [];
-
-// function isNumberKey(evt){
-//     var charCode = (evt.which) ? evt.which : evt.keyCode
-//     if (charCode > 31 && (charCode < 48 || charCode > 57))
-//         return false;
-//     return true;
-// }
-
-// const selectCell = (event) => {
-//     const cells = grid.querySelectorAll('.cell');
-//     cells.forEach((cell) => {
-//         cell.classList.remove('selected');
-//     });
-//     const selectedCell = event.currentTarget;
-//     selectedCell.classList.add('selected');
-// }
 
 const acceptInput = (event) => {
     const InputBoxInCell = event.currentTarget;
@@ -30,15 +11,8 @@ const acceptInput = (event) => {
 
     if (['1', '2', '3', '4', '5', '6', '7', '8', '9'].includes(cellInput)) {
         gridCell.textContent = cellInput;
-
         console.log(gridCell);
-
-        // gridCell.removeChild(InputBoxInCell);
         gridCell.addEventListener('click', createInputBox);
-
-        // gridCell.insertAdjacentHTML('beforeend', '<input>'); // ADDED FOR TESTING
-        // gridCell.querySelector('input').addEventListener('keyup', acceptInput); // ADDED FOR TESTING
-        // gridCell.querySelector('input').style.display = 'none';
         checkIfSolved();
     } else {
         gridCell.textContent == '';
@@ -46,7 +20,6 @@ const acceptInput = (event) => {
 }
 
 const createInputBox = (event) => {
-
     // Delete any other input boxes in the grid
     const rows = grid.querySelectorAll('tr');
     // Iterate through all board rows
@@ -62,36 +35,13 @@ const createInputBox = (event) => {
             }
         }
     }
-
     if (event.currentTarget.querySelector('input') === null) {
-
         console.log(event.currentTarget);
-
         newInputBox = document.createElement('input');
         newInputBox.addEventListener('keyup', acceptInput);
-        // newInputBox.textContent = event.currentTarget.textContent;
         newInputBox.value = event.currentTarget.textContent;
-
         event.currentTarget.textContent = "";
-
-        // console.log(newInputBox);
-
-        // event.currentTarget.appendChild(newInputBox);
-        // event.currentTarget.insertAdjacentHTML('beforeend', newInputBox);
         event.currentTarget.appendChild(newInputBox);
-
-        // console.log(event.currentTarget.innerHTML);
-
-        // const InputBoxInCell = event.currentTarget.querySelector('input');
-
-        // console.log(InputBoxInCell);
-
-        // InputBoxInCell.addEventListener('keyup', acceptInput);
-
-        // event.currentTarget.textContent = '';
-
-        // console.log(event.currentTarget);
-
     }
 }
 
@@ -127,17 +77,7 @@ const resetGrid = () => {
             rowCell.setAttribute('column_index', j);
             if (newGrid[i][j] === 0) {
                 rowCell.setAttribute('blank', 'true');
-                // rowCell.addEventListener('click', selectCell);
-                // rowCell.addEventListener('click', createInputBox);
-                // rowCell.insertAdjacentHTML('beforeend', '<input>'); // ADDED FOR TESTING
-                // rowCell.querySelector('input').addEventListener('keyup', acceptInput);
-
-                // rowCell.addEventListener('click', (event) => {
-                //     console.log('clicked');
-                //     rowCell.querySelector('input').style.display = 'initial';
-                // })
                 rowCell.addEventListener('click', createInputBox);
-
                 // rowCell.addEventListener('arrow keys', move);
             } else {
                 rowCell.setAttribute('blank', 'false');
@@ -160,7 +100,6 @@ const validateInput = (gridCell) => {
     const rows = grid.querySelectorAll('tr');
     const row = rows[rowIdx];
     const cells = row.querySelectorAll('td');
-    // const gridCell = cells[j];
 
     // find all other numbers in cell row
     const cellRow = [];
@@ -168,8 +107,6 @@ const validateInput = (gridCell) => {
         cellRow.push(cells[k].textContent);
     }
     cellRow.splice(columnIdx, 1);
-    // console.log('cellRow:');
-    // console.log(cellRow);
 
     // find all other numbers in cell column
     const cellCol = [];
@@ -178,46 +115,30 @@ const validateInput = (gridCell) => {
         cellCol.push(tds[columnIdx].textContent);
     }
     cellCol.splice(rowIdx, 1);
-    // console.log('cellCol:');
-    // console.log(cellCol);
 
     // find all other numbers in cell square
     // FIX THIS -------- THINK IT'S FIXED
     const cellSquare = [];
     const squareRowIdx = (Math.floor((parseInt(rowIdx))/3.0) * 3);
-    // console.log('square row idx');
-    // console.log(squareRowIdx);
-    // console.log('column idx');
-    // console.log(columnIdx);
     const squareColumnIdx = (Math.floor(parseInt(columnIdx)/3.0) * 3);
-    // console.log('square column idx');
-    // console.log(squareColumnIdx);
     for (m = squareRowIdx; m < squareRowIdx + 3; m++) {
-        // console.log('m');
-        // console.log(m);
         const td_rows = rows[m].querySelectorAll('td');
         for (n = squareColumnIdx; n < squareColumnIdx + 3; n++) {
-            // console.log('n');
-            // console.log(n);
-            // if (rowIdx != m && columnIdx != n) {
-            // console.log(td_rows[n].textContent);
             cellSquare.push(td_rows[n].textContent);
             // }
         }
     }
     squareIdx = ((rowIdx % 3) * 3) + (columnIdx % 3);
-    // console.log(squareIdx);
     cellSquare.splice(squareIdx, 1);
-    // console.log('cellSquare:');
-    // console.log(cellSquare);
-
 
     // if the cell's number isn't duplicated in its row, column or square, color it blue
-    // console.log('input:');
+
+    // DELETE ONCE FEATURE IS COMPLETE:
     console.log(input);
     console.log(cellRow);
     console.log(cellCol);
     console.log(cellSquare);
+
     // if (cellRow.includes(`${input}`) === false && cellCol.includes(`${input}`) === false) {
     if (cellRow.includes(`${input}`) === false && cellCol.includes(`${input}`) === false && cellSquare.includes(`${input}`) === false) {
         console.log('Returning true...');
@@ -229,7 +150,6 @@ const validateInput = (gridCell) => {
 }
 
 const checkIfSolved = () => {
-    // let boardSolved = true;
     grid.setAttribute('solved', 'true');
 
     // iterate through every cell in the grid and check whether input value is valid
@@ -248,14 +168,10 @@ const checkIfSolved = () => {
                 } else {
                   gridCell.style.color = "#ff0100";
                     grid.setAttribute('solved', 'false');
-                    // let boardSolved = false;
-                    // console.log(boardSolved);
                 }
             }
         }
     }
-    // console.log(boardSolved);
-    // if (boardSolved === true) {
     if (grid.getAttribute('solved') === 'true') {
         alert('You won!');
         resetGrid();
@@ -274,8 +190,6 @@ const restart = () => {
             const gridCell = cells[j];
             if (gridCell.getAttribute('blank') === 'true') {
                 gridCell.textContent = "";
-                // gridCell.insertAdjacentHTML('beforeend', '<input>');
-                // gridCell.querySelector('input').addEventListener('keyup', acceptInput);
             }
         }
     }
